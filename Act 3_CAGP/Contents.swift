39584f642096fa54f92bd953fac7b46da31e8a65
XtianDoubty <89282840+xtiandoubty@users.noreply.github.com>
2021-10-06 19:42:38 -0500

    Add files via upload

---
+++ Act 3_CAGP/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var Entero: Int = 55
+var Decimal: Float = 3.141592654
+var str = "Me llamo Christian"
+var Real: Bool = true
+
+var ciudad1 = "Caracas"
+var cantidadHijos = 3
+
+var numeros:Array<Int> = Array<Int>()
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+numeros.append(4)
+numeros.append(5)
+numeros.append(6)
+numeros.append(7)
+numeros.append(8)
+numeros.append(9)
+numeros.append(10)
+numeros.count
+
+var diasSemana:Dictionary<String, Int> = Dictionary<String, Int>()
+diasSemana = ["lunes": 1]
+diasSemana = ["martes": 2]
+diasSemana = ["miercoles": 3]
+diasSemana = ["jueves": 4]
+diasSemana = ["viernes": 5]
+diasSemana = ["sabado": 6]
+diasSemana = ["domingo": 7]
+print("Diccionario: \(diasSemana)")
