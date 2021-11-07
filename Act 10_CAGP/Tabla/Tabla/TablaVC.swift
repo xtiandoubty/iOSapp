dd58cb42ea51d00c57d8d3d924be647c06e78791
XtianDoubty <89282840+xtiandoubty@users.noreply.github.com>
2021-11-06 19:31:12 -0600

    Add files via upload

---
+++ Act 10_CAGP/Tabla/Tabla/TablaVC.swift
@@ -0,0 +1,94 @@
+//
+//  TablaVC.swift
+//  Tabla
+//
+//  Created by L01451229 on 4/28/16.
+//  Copyright © 2016 itesm.mx. All rights reserved.
+//
+
+import UIKit
+
+class TablaVC: UITableViewController {
+    
+    let nombres = ["Carlos", "Luis", "Pedro", "Jose"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    // MARK: - Table view data source
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return nombres.count
+    }
+
+   
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath as IndexPath)
+        // Configurar celda
+        //Configurar celda el código cell.textLabel?.text = nombres [indexPath.row]
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "verDetalle"{
+            let _:ViewController = segue.destination as! ViewController
+        }
+        
+    }
+    
+
+}
---
+++ Act 10_CAGP/Tabla/Tabla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tabla
+//
+//  Created by L01451229 on 4/28/16.
+//  Copyright © 2016 itesm.mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
