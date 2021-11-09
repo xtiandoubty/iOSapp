5b42464989167439a5f3b94a76fc31c36370a7f7
XtianDoubty <89282840+xtiandoubty@users.noreply.github.com>
2021-11-09 15:26:34 -0600

    Add files via upload

---
+++ Act 8_CAGP/Act8/Prototipo/Prototipo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prototipo
+//
+//  Created by user190613 on 11/3/21.
+//  Copyright © 2021 user190613. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
